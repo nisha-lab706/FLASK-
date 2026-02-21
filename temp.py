7a1717d1c13ddffd4c1e44ac0d399053bf58d79e
nisha-lab706 <nishalangote359@gmail.com>
2026-02-21 10:12:33 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,32 @@
+import requests
+
+apikey="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"
+word = input("Enter your word: ")
+
+res = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+
+print("status_code:",res.status_code)
+
+if res.status_code == 200:
+    meanings =res.json()[0]["meanings"]
+    for meaning in meanings:
+        defn = meaning['definitions']
+        for d in defn:
+             print(d['definition'])
+        else:
+            print("word not found in dictionary")
+
+        
+              
+             
+
+
+
+
+    
+    
+        
+
+        
+
+
